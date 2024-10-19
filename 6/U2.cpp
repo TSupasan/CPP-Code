7fd9d76d6bc7392440a8b3e94630ec61a8eaf5c1
Tharindu Supasan Aththanayaka <tharindu.supasan@gmail.com>
2024-10-19 20:05:46 +0530

    Programme without "using namespace std;"
    When you don't use using namespace std;, you need to explicitly prefix standard library objects and functions with std::

---
+++ 6/U2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    string city,country;
+    std::cout<<"Enter the city\n";
+    std::cin>>city;
+    std::cout<<"Enter the country name\n";
+    std::cin>>country;
+    std::cout<<"You live in "<<city<<","<<country;
+    return 0;
+}
+

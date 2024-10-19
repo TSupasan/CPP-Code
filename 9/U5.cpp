b0354a6764144101ee03fa34ef1b264d6cfd651f
Tharindu Supasan Aththanayaka <tharindu.supasan@gmail.com>
2024-10-19 20:16:54 +0530

    Checking main data types in c++
    1.int: Represents integers (whole numbers) like 5, -10, 100.
    
    2.float: Represents single-precision floating-point numbers (decimal values) like 3.14 or -0.5.
    
    3.double: Represents double-precision floating-point numbers for more precise decimals, like 3.14159.
    
    4.char: Represents a single character, like 'A' or '1'.
    
    5.bool: Represents a boolean value, either true or false.
    
    6.string: Represents a sequence of characters (text), like "Hello".

---
+++ 9/U5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    float height;
+    double weight;
+    char letter;
+    string name;
+    bool f;
+
+    cout<<"Enter your age"<<endl;
+    getline(cin,age);
+
+    cout<<"Enter you height"<<endl;
+    getline(cin.height);
+
+    cout<<"Enter your weight"<<endl;
+    getline(cin.weight);
+
+    cout<<"What is your favourite letter?"<<endl;
+    getline(cin,letter);
+
+    cout<<"what is your name?"<<endl;
+    getline(cin.name);
+
+    cout<<"hello "<<name<<" You are "<<age<<" years old\n"<<"Your Height = "<<height<<"Your Weight = "<<weight
+
+    return 0;
+
+}

0da41785c5b7b639b8a2c534692dd12e59c847dc
Tharindu Supasan Aththanayaka <tharindu.supasan@gmail.com>
2024-11-23 11:53:17 +0530

    Rectangle Area and Perimeter Calculator
    This C++ code effectively calculates the area and perimeter of a rectangle. It prompts the user to input the length and width, applies the standard formulas, and displays the results.

---
+++ 13.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double L,W;
+    cout<<"Enter the Length: "<<endl;
+    cin>>L;
+    cout<<"Enter the Width: "<<endl;
+    cin>>W;
+
+    cout<<"Area of the Rectangle = "<<(L*W)<<endl;
+    cout<<"Perimeter of the Rectangle = "<<(L+W)*2;
+    return 0;
+}

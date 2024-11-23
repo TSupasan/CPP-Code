0c81ed038dd4925f6df16c8830627904a0dcab79
Tharindu Supasan Aththanayaka <tharindu.supasan@gmail.com>
2024-11-23 11:59:33 +0530

    String Length Calculation
    This C++ code determines the length of a given string input by the user. It utilizes the getline function to read the entire string, including spaces, and employs the length() function to calculate the number of characters. The calculated length is then printed to the console.

---
+++ 17.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string x;
+    cout<<"Enter the string : ";
+    getline(cin,x);
+    int n=x.length();
+    cout<<"number of character of x :"<<n;
+    return 0;
+
+
+}

55f8dab559ec893ec68f8a15c8e54364940070d8
Tharindu Supasan Aththanayaka <tharindu.supasan@gmail.com>
2024-11-30 09:55:47 +0530

    Conditional Statements with if-else in C++
    This C++ code demonstrates the use of if-else statements to make decisions based on a user-input number. If the number is greater than 5, the code prints "This number is higher than 5." Otherwise, it prints "This number is lower than 5." Regardless of the condition, the code always prints "The END" at the end.

---
+++ 28.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Enter n umber : ";
+    cin>>number;
+
+    if (number > 5 ){
+        cout << "This number is higher than 5"<<endl;
+    }
+    else {
+        cout << "This number is lower than 5"<<endl;
+    }
+    cout << "The END";
+    return 0;
+}
+

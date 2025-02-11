fbedf70a0a9eec57ae39b5376d80fd9f8b7213a0
Tharindu Supasan Aththanayaka <tharindu.supasan@gmail.com>
2025-02-11 20:40:55 +0530

    Printing Elements of an Array
    Description:  
    This C++ program demonstrates how to print elements of an array.  
    - An array `arr[6]` is declared and initialized with six integer values: `{2, 4, 5, 6, 7, 8}`.  
    - A `for` loop is used to iterate through the array, printing the index and the corresponding element for each iteration.  
    - The output displays the index and element of the array.
    
    Special Tags Used:  
    - int array → Declares and initializes an integer array.  
    - for loop → Iterates through the array to print its elements.  
    - cout → Outputs the index and value of each array element.
    
    Example Output:  
    Print the elements  
    index 0 elements is:2  
    index 1 elements is:4  
    index 2 elements is:5  
    index 3 elements is:6  
    index 4 elements is:7  
    index 5 elements is:8

---
+++ 54.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[6]={2,4,5,6,7,8};
+    cout<<"Print the elements"<<endl;
+    for(int i=0 ; i<6; i++)
+    {
+        cout<<"index "<<i<<" elements is:"<<arr[i]<<endl;
+    }
+    return 0;
+}

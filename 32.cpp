cb3da58fb41efeec58a9aee1ca7380fb5684ed67
Tharindu Supasan Aththanayaka <tharindu.supasan@gmail.com>
2025-02-11 19:43:42 +0530

    For Loop Example in C++
     This C++ program demonstrates the use of a for loop 
       to print numbers from 0 to 5. 
       - Initialization (int i = 0): Sets the starting value.
       - Condition (i <= 5): Runs the loop while true.
       - Updation (i++): Increments i after each iteration.
       
       Special Tags Used:
       - #include<iostream>: Includes input-output stream.
       - using namespace std;: Allows standard library usage.
       - for loop: Used for iteration.
       - cout: Prints output to the console.

---
+++ 32.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* for ( initialization; test condition; updation )
+    //body of for loop*/
+
+        for(int i=0 ; i<=5 ; i++)
+        {
+            cout<<i<<endl;
+        }
+
+    return 0;
+}
